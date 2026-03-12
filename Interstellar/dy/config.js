54cbe76e1e7060f99ff0c14123bdde16d2c44966
Dragonjr123 <95893112+Dragonjr123@users.noreply.github.com>
2026-03-11 21:14:56 -0400

    ahhh

--- Interstellar/dy/config.js
+++ Interstellar/dy/config.js
@@ -5,7 +5,7 @@ self.__dynamic$config = {
   logLevel: 0,
   bare: {
     version: 2,
-    path: "/ov/",
+    path: "/api/ov/",
   },
   tab: {
     title: null,
--- packages/scratch-gui/static/interstellar/assets/-/config.js
+++ packages/scratch-gui/static/interstellar/assets/-/config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/interstellar/a/',
-    bare: '/ov/',
+    bare: '/api/ov/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/interstellar/assets/-/handler.js?v=5-5-2024',
